f374ef1da1504d71c89d8ededf8b2a8bd356ea15
JohnTSalmon <104514061+JohnTSalmon@users.noreply.github.com>
2026-03-02 14:19:17 +0000

    Add Logger class for logging messages

---
+++ src/main/java/com/mercator/logger/Logger.java
@@ -0,0 +1,15 @@
+package com.mercator.logger;
+
+public class Logger {
+    private static java.util.logging.Logger logger = null;
+
+    public Logger() {
+        logger = java.util.logging.Logger.getLogger(this.getClass().getName());
+    }
+    public static void log(String msg ) {
+        logger.info(msg);
+    }
+    public static void warn(String msg ) {
+        logger.warning(msg);
+    }
+}
